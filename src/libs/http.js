05239888ed9d87c077cf63f54fc91d83e394ad3e
Marcos Rivas Rojas <marcosrivasr@gmail.com>
2021-04-23 13:05:43 -0500

    put method implemented

--- src/libs/http.js
+++ src/libs/http.js
@@ -12,6 +12,18 @@ export async function post(url, data){
     return json;
 }
 
+export async function put(url, data){
+    const response = await fetch(url, {
+                        method: 'put', 
+                        headers:{
+                          'content-type': 'application/json'
+                        },
+                        body: JSON.stringify(data)
+                      });
+    const json = await response.json();
+    return json;
+}
+
 export async function get(url){
     const response = await fetch(url);
     const json = await response.json();
