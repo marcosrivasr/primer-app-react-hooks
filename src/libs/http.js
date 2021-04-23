9ecf6f2cd99a4f7fcbd8e36e3cb6036f3dd0df5d
Marcos Rivas Rojas <marcosrivasr@gmail.com>
2021-04-23 11:34:26 -0500

    http request module created

---
+++ src/libs/http.js
@@ -0,0 +1,19 @@
+
+
+export async function post(url, data){
+    const response = await fetch(url, {
+                        method: 'post', 
+                        headers:{
+                          'content-type': 'application/json'
+                        },
+                        body: JSON.stringify(data)
+                      });
+    const json = await response.json();
+    return json;
+}
+
+export async function get(url){
+    const response = await fetch(url);
+    const json = await response.json();
+    return json;
+}
